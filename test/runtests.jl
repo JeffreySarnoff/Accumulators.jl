b77a9c12e42efe75ac5f24ac8a3cdd195ad49177
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-11-02 04:59:10 -0400

    Update runtests.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,6 +2,12 @@ using Accumulators, StatsBase, Test
 
 include("testvals.jl")
 
+@inline function accvals(acc, vals)
+    for x in vals
+        acc(x)
+    end
+end
+
 @testset "compose" begin
     include("compose.jl")
 end
