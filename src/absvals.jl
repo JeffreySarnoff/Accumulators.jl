707ad575bdb447872a87698fa2285e1b47443d9f
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-11-02 02:58:51 -0400

    Update absvals.jl

--- src/absvals.jl
+++ src/absvals.jl
@@ -1,10 +1,10 @@
-AccMinAbs(::Type{T}) = AccumMin(T, abs)
-AccMaxAbs(::Type{T}) = AccumMax(T, abs)
-AccMeanAbs(::Type{T}) = AccumMean(T, abs)
-AccMeanAbs2(::Type{T}) = AccumMean(T, abs2)
+AccMinAbs(::Type{T}) where {T} = AccumMin(T, abs)
+AccMaxAbs(::Type{T}) where {T} = AccumMax(T, abs)
+AccMeanAbs(::Type{T}) where {T} = AccumMean(T, abs)
+AccMeanAbs2(::Type{T}) where {T} = AccumMean(T, abs2)
 
-AccSumAbs(::Type{T}) = AccumSum(T, abs)
-AccProdAbs(::Type{T}) = AccumProd(T, abs)
+AccSumAbs(::Type{T}) where {T} = AccumSum(T, abs)
+AccProdAbs(::Type{T}) where {T} = AccumProd(T, abs)
 
-AccMeanVarAbs(::Type{T}) = AccumMeanVar(T, abs)
-AccMeanVarAbs2(::Type{T}) = AccumMeanVar(T, abs2)
+AccMeanVarAbs(::Type{T}) where {T} = AccumMeanVar(T, abs)
+AccMeanVarAbs2(::Type{T}) where {T} = AccumMeanVar(T, abs2)
