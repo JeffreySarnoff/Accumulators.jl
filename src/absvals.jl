0b6aa2ad8979d55aef6b46478f05854338bddd32
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-11-02 04:16:35 -0400

    Update absvals.jl

--- src/absvals.jl
+++ src/absvals.jl
@@ -1,10 +1,17 @@
 AccMinAbs(::Type{T}) where {T} = AccumMin(T, abs)
+AccMinAbs2(::Type{T}) where {T} = AccumMin(T, abs2)
+
 AccMaxAbs(::Type{T}) where {T} = AccumMax(T, abs)
+AccMaxAbs2(::Type{T}) where {T} = AccumMax(T, abs2)
+
 AccMeanAbs(::Type{T}) where {T} = AccumMean(T, abs)
 AccMeanAbs2(::Type{T}) where {T} = AccumMean(T, abs2)
 
 AccSumAbs(::Type{T}) where {T} = AccumSum(T, abs)
+AccSumAbs2(::Type{T}) where {T} = AccumSum(T, abs2)
+
 AccProdAbs(::Type{T}) where {T} = AccumProd(T, abs)
+AccProdAbs2(::Type{T}) where {T} = AccumProd(T, abs2)
 
 AccMeanVarAbs(::Type{T}) where {T} = AccumMeanVar(T, abs)
 AccMeanVarAbs2(::Type{T}) where {T} = AccumMeanVar(T, abs2)
