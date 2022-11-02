8c5ca6be8dd594bda07b530c4dc9727bb912f504
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-11-02 04:37:50 -0400

    Update compose.jl

--- test/compose.jl
+++ test/compose.jl
@@ -1,4 +1,26 @@
 @testset "simple accumulators" begin
+   acc = AccCount()
+   for x in one2five
+       acc(x)
+   end
+   @test acc() == 5
+  
+   acc = AccMin()
+   for x in sortn8
+       acc(x)
+   end
+   @test acc() == sortn8[1]
+  
+   acc = AccMax()
+   for x in sortn8
+       acc(x)
+   end
+   @test acc() == sortn8[end]
 
+   acc = AccExtrema()
+   for x in sortn8
+       acc(x)
+   end
+   @test acc() == (sortn8[1], sortn8[end]
 end
 
