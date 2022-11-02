e8766ebd78018b1fbf4b82db5ebee17f6336d9dc
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-11-02 04:50:05 -0400

    Update compose.jl

--- test/compose.jl
+++ test/compose.jl
@@ -1,39 +1,42 @@
+@inline function accvals(acc, vals)
+    for x in vals
+      acc(x)
+    end
+end
+
 @testset "simple accumulators" begin
    acc = AccCount()
-   for x in one2five
-       acc(x)
-   end
+   accvals(acc, one2five)
    @test acc() == 5
   
    acc = AccMin()
-   for x in sortn8
-       acc(x)
-   end
+   accvals(acc, sortn8)
    @test acc() == sortn8[1]
   
    acc = AccMax()
-   for x in sortn8
-       acc(x)
-   end
+   accvals(acc, sortn8)
    @test acc() == sortn8[end]
 
    acc = AccExtrema()
-   for x in sortn8
-       acc(x)
-   end
+   accvals(acc, sortn8)
    @test acc() == (sortn8[1], sortn8[end])
 end
 
 @testset "sum, prod" begin
     acc = AccSum()
-    for x in sortn8
-      acc(x)
-    end
+    accvals(acc, sortn8)
     @test acc() == foldl(+, sortn8; init=0.0)
 
     acc = AccProd()
-    for x in sortn8
-      acc(x)
-    end
+    accvals(acc, sortn8)
     @test acc() == foldl(*, sortn8; init=1.0)
 end
+
+@testset "means" begin
+    acc = AccMean()
+    accvals(acc, randn8)
+    @test acc() == mean(randn8)
+   
+end
+   
+   
