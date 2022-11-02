c6b4db60f6236ae9b66bd06b6b32680c4724a7c9
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-11-02 07:58:07 -0400

    Update constants.jl

--- test/constants.jl
+++ test/constants.jl
@@ -1,11 +1,11 @@
-const one2five = (1.0, 2.0, 3.0, 4.0, 5.0)
-const one2fifty = Tuple(collect(1.0:50.0))
+const one2five = (1.0, 2.0, 3.0, 4.0, 5.0);
+const one2fifty = Tuple(collect(1.0:50.0));
 
-r = randn(8)
-const sortn8 = sort(r)
-const randn8 = r
+r = randn(8);
+const sortn8 = sort(r);
+const randn8 = r;
 
-r = randn(48)
-const sortn48 = sort(r)
-const randn48 = r
+r = randn(48);
+const sortn48 = sort(r);
+const randn48 = r;
 
