e0c857088f4bcfe29e679b3cda6a40445e7d228d
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-11-13 07:15:29 -0500

    Update short_memories.jl

--- src/short_memories.jl
+++ src/short_memories.jl
@@ -7,7 +7,7 @@
 mutable struct AccCount{T} <: Accumulator{T}
     n⬭::Int
 
-    function AccCount(n::T=0) where {T}
+    function AccCount(; n::T=0) where {T}
         new{T}(n)
     end
 end
@@ -30,23 +30,31 @@ end
 # Short Memory, accumulate observations, spilling oldest once filled
 mutable struct AccObs{T, N} <: Accumulator{T}
     n⬭::Int
-    obs⬭::Tuple{N,T}
-
-    function AccObs(n::Int=0, obs::NTuple{N,T}=ntuple(∅->0, N)) where {T, N}
-        new{T, N}(n, obs)
-    end
+    obs⬭::NTuple{N,T}
 end
 
-function (acc::AccCount{T})() where {T}
-    acc.n⬭
+function AccObs{T}(nobs::Int=1) where {T}
+     init_obs = ntuple(∅->zero(T), nobs)
+     AccObs(0, init_obs)
+end
+     
+function (acc::AccObs{T, N})() where {T, N}
+    acc.obs⬭
 end
 
-function (acc::AccCount{T})(x) where {T}
+function (acc::AccObs{T, 2})(x) where {T}
     acc.n⬭ += 1
+    acc.obs⬭ = (acc.obs⬭[2], x)
     acc
 end
 
-function (acc::AccCount{T})(xs::Seq) where {T}
+function (acc::AccObs{T, N})(x) where {T, N}
+    acc.n⬭ += 1
+    acc.obs⬭ = (acc.obs⬭[1:N-1]..., x)
+    acc
+end
+
+function (acc::AccObs{T, N})(xs::Seq) where {T, N}
     acc.n⬭ += length(xs)
     acc
 end
