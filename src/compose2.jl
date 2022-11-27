cb4fe86784dfab13342014beeb944fa0f1bdb449
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-11-27 04:54:01 -0500

    Update compose2.jl

--- src/compose2.jl
+++ src/compose2.jl
@@ -16,6 +16,8 @@ def online_covariance(data1, data2):
     sample_covar = C / (n - 1)
 =#
 
+# Cov
+
 mutable struct AccCov{T} <: Accumulator{T}
     nobs::Int
     xmean::T
@@ -49,6 +51,59 @@ function (acc::AccCov{T})(xs::Seq{T}, ys::Seq{T}) where {T}
     ymean = vmean(ys)
     dy = ymean - acc.ymean
     acc.ymean += dy / acc.nobs
+    acc.c += dx * dy
+    acc
+end
+
+# Corr
+
+mutable struct AccCorr{T} <: Accumulator{T}
+    nobs::Int
+    xmean::T
+    ymean::T
+    xsvar::T
+    ysvar::T
+    c::T
+end
+
+function AccCorr(::Type{T}=Float64) where {T}
+    AccCov{T}(0, zero(T), zero(T), zero(T), zero(T), zero(T))
+end
+
+function (acc::AccCorr{T})() where {T}
+    corr = acc.c / (acc.nobs - 1)
+    unbiased_xvar = acc.xsvar / (acc.nobs - 1)
+    unbiased_yvar = acc.ysvar / (acc.nobs - 1)
+    corr / (unbiased_xvar * unbiased_yvar)
+end
+
+function (acc::AccCov{T})(x, y) where {T}
+    acc.nobs += 1
+    prior_xmean = acc.xmean
+    dx = x - prior_xmean
+    acc.xmean += dx / acc.nobs
+    acc.xsvar = acc.xsvar + (x - prior_xmean) * (x - acc.xmean)
+    prior_ymean = acc.ymean
+    dy = y - acc.ymean
+    acc.ymean += dy / acc.nobs
+    acc.ysvar = acc.ysvar + (y - prior_ymean) * (y - acc.ymean)
+    acc.c += dx * dy
+    acc
+end
+
+function (acc::AccCov{T})(xs::Seq{T}, ys::Seq{T}) where {T}
+    if length(xs) !== length(ys) 
+        throw(ArgumentError(string("lengths must be equal (", n, " != ", length(ys), ")")))
+    end
+    acc.nobs += length(xs)
+    xmean = vmean(xs)
+    dx = xmean - acc.xmean
+    acc.xmean += dx / acc.nobs
+    acc.xsvar = acc.xsvar + (x - prior_xmean) * (x - acc.xmean)
+    ymean = vmean(ys)
+    dy = ymean - acc.ymean
+    acc.ymean += dy / acc.nobs
+    acc.ysvar = acc.ysvar + (y - prior_ymean) * (y - acc.ymean)
     acc.c += dx * dy
     acc
 end
