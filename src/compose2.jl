f141eb1bbcad4f2e7382031f2cb91fdb64d60e30
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-11-27 02:37:54 -0500

    Create compose2.jl

---
+++ src/compose2.jl
@@ -0,0 +1,71 @@
+# compose with two data streams
+
+#=
+
+def online_covariance(data1, data2):
+    meanx = meany = C = n = 0
+    for x, y in zip(data1, data2):
+        n += 1
+        dx = x - meanx
+        meanx += dx / n
+        meany += (y - meany) / n
+        C += dx * (y - meany)
+
+    population_covar = C / n
+    # Bessel's correction for sample variance
+    sample_covar = C / (n - 1)
+=#
+
+mutable struct AccCov{T} <: Accumulator{T}
+    nobs::Int
+    xmean::T
+    ymean::T
+    c::T
+end
+
+function AccCov(::Type{T}=Float64) where {T}
+    AccCov{T}(0, zero(T), zero(T), zero(T))
+end
+
+function (acc::AccCov{T})() where {T}
+    acc.c / (acc.nobs - 1)
+end
+
+function (acc::AccCov{T})(x, y) where {T}
+    acc.nobs += 1
+    dx = x - acc.xmean
+    dy = y - acc.ymean
+    acc.xmean += dx / acc.nobs
+    acc.ymean += dy / acc.nobs
+    acc.c += dx * dy
+    acc
+end
+
+function (acc::AccCov{T})(xs::A, ys::A) where {T, A<:AbstractVector{T}}
+    acc.nobs += length(xs)
+    prior_xmean = acc.xmean
+    prior_ymean = acc.ymean
+    xmean = vmean(xs)
+    ymean = vmean(ys)
+    dx = xmean - prior_xmean
+    dy = ymean - prior_ymean
+    acc.xmean += dx / acc.nobs
+    acc.ymean += dy / acc.nobs
+    acc.c += dx * dy
+    acc
+end
+
+function (acc::AccCovar{T})(xs::NTuple{N,T}, ys::NTuple{N,T}) where {T, N}
+    acc.nobs += N
+    prior_xmean = acc.xmean
+    prior_ymean = acc.ymean
+    xmean = vmean(xs)
+    ymean = vmean(ys)
+    dx = xmean - prior_xmean
+    dy = ymean - prior_ymean
+    acc.xmean += dx / acc.nobs
+    acc.ymean += dy / acc.nobs
+    acc.c += dx * dy
+    acc
+end
+
