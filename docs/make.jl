ed055cc6eb0702eaef6506f279df97c9de8caf61
Jeffrey Sarnoff <JeffreySarnoff@users.noreply.github.com>
2022-12-06 00:47:56 -0500

    Create make.jl

---
+++ docs/make.jl
@@ -0,0 +1,18 @@
+using Documenter, Accumulators
+
+makedocs(
+    modules = [Accumulators],
+    sitename = "Accumulators.jl",
+    authors = "Jeffrey A. Sarnoff <jeffrey.sarnoff@gmail.com>",
+    pages = Any[
+        "Overview" => "index.md",
+        "Guide" => "guide.md",
+        "Supported Operations" => "supports.md",
+        "Refs" => "references.md"
+    ]
+)
+
+deploydocs(
+    repo = "github.com/JeffreySarnoff/Accumulators.jl.git",
+    target = "build"
+)
