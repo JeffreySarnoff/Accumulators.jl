eb383819c29fc409c50921e755722f0bd04082c2
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-12-31 22:59:17 -0500

    cache updating

--- src/short_memories.jl
+++ src/short_memories.jl
@@ -4,57 +4,98 @@
 
 
 # Count
-mutable struct AccCount{T} <: Accumulator{T}
-    n⬭::Int
+mutable struct AccumCount{T,F} <: AccumCacher{T,F}
+    nᵪ::Int
 
-    function AccCount(; n::T=0) where {T}
-        new{T}(n)
+    function AccumCount(; n::T=0) where {T,F}
+        new{T,F}(n)
     end
 end
 
-function (acc::AccCount{T})() where {T}
-    acc.n⬭
+function (accum::AccumCount{T,F})() where {T,F}
+    accum.nᵪ
 end
 
-function (acc::AccCount{T})(x) where {T}
-    acc.n⬭ += 1
-    acc
+function (accum::AccumCount{T,F})(x) where {T,F}
+    accum.nᵪ += 1
+    accum
 end
 
-function (acc::AccCount{T})(xs::Seq) where {T}
-    acc.n⬭ += length(xs)
-    acc
+function (accum::AccumCount{T,F})(xs::Seq) where {T,F}
+    accum.nᵪ += length(xs)
+    accum
 end
 
 
-# Short Memory, accumulate observations, spilling oldest once filled
-mutable struct AccObs{T, N} <: Accumulator{T}
-    n⬭::Int
-    obs⬭::NTuple{N,T}
+# Short Memory, accumumulate observations, spilling oldest once filled
+mutable struct AccumObs{T,F,N} <: AccumCacher{T,F}
+    nᵪ::Int
+    obsᵪ::NTuple{N,T}
 end
 
-function AccObs{T}(nobs::Int=1) where {T}
-     init_obs = ntuple(∅->zero(T), nobs)
-     AccObs(0, init_obs)
-end
-     
-function (acc::AccObs{T, N})() where {T, N}
-    acc.obs⬭
+function AccumObs{T,F}(nobs::Int=1) where {T,F}
+    init_obs = ntuple(∅ -> zero(T), nobs)
+    AccumObs(0, init_obs)
 end
 
-function (acc::AccObs{T, 2})(x) where {T}
-    acc.n⬭ += 1
-    acc.obs⬭ = (acc.obs⬭[2], x)
-    acc
+function (accum::AccumObs{T,F,N})() where {T,F,N}
+    accum.obsᵪ
 end
 
-function (acc::AccObs{T, N})(x) where {T, N}
-    acc.n⬭ += 1
-    acc.obs⬭ = (acc.obs⬭[1:N-1]..., x)
-    acc
+function (accum::AccumObs{T,2})(x) where {T,F}
+    accum.nᵪ += 1
+    accum.obsᵪ = (accum.obsᵪ[2], x)
+    accum
 end
 
-function (acc::AccObs{T, N})(xs::Seq) where {T, N}
-    acc.n⬭ += length(xs)
-    acc
+function (accum::AccumObs{T,F,N})(x) where {T,F,N}
+    accum.nᵪ += 1
+    accum.obsᵪ = (accum.obsᵪ[1:N-1]..., x)
+    accum
+end
+
+function (accum::AccumObs{T,F,N})(xs::Seq) where {T,F,N}
+    accum.nᵪ += length(xs)
+    accum
+end
+
+
+# Minimum
+
+mutable struct AccumMinimum1{T,F} <: AccumCacher{T,F}
+    obsᵪ::NTuple{N,T}   # cache most recent N obs
+    const fn::F         # preapply to each element when observed
+    nobsᵪ::Int          # count each observation
+    nmin::Int           # count distinct minima
+    min::T              # current minimum
+end
+
+function AccumMinimum1(::Type{T}=AccumNum; fn::F=identity, ncached=4) where {T,F}
+    cache = ntuple(i -> zero(T), ncached)
+    AccMinimum1{T,F}(cache, 0, 0, typemax(T), fn)
+end
+
+function (acc::AccMinimum{T,F})() where {T,F}
+    acc.min
+end
+
+function (acc::AccMinimum{T,F})(x::T) where {T,F}
+    xx = acc.fn(x)
+    acc.nobs += 1
+    if x < acc.min
+        acc.nmin += 1
+        acc.min = xx
+    end
+    accum
+end
+
+function (acc::AccMinimum{T,F})(xs::Seq{T}) where {T,F}
+    xxs = map(acc.fn, xs)
+    acc.nobs += length(xs)
+    x = vminimum(xxs)
+    if x < acc.min
+        acc.nmin += 1
+        acc.min = x
+    end
+    accum
 end
--- src/support.jl
+++ src/support.jl
@@ -1,4 +1,39 @@
-const AccNum = Float64
+const AccNum = F
+
+# shift new obs into tuple, drop oldest obs
+#=
+    init_cache   = (x₀, x₀, x₀, x₀)
+    new_obs = x₁
+    update_cache = (x₀, x₀, x₀, x₁)
+    new_obs = x₂
+    update_cache = (x₀, x₀, x₁, x₂)
+    new_obs = x₃
+    update_cache = (x₀, x₁, x₂, x₃)
+    new_obs = x₄
+    update_cache = (x₁, x₂, x₃, x₄)
+    new_obs = x₅
+    update_cache = (x₂, x₃, x₄, x₅)
+    new_obs = x₆
+    update_cache = (x₃, x₄, x₅, x₆)
+
+
+    init_cache   = (x₀, x₀, x₀, x₀)
+    new_obs = x₁
+    update_cache = (x₁, x₀, x₀, x₀)
+    new_obs = x₂
+    update_cache = (x₂, x₁, x₀, x₀)
+    new_obs = x₃
+    update_cache = (x₃, x₂, x₁, x₀)
+    new_obs = x₄
+    update_cache = (x₄, x₃, x₂, x₁)
+    new_obs = x₅
+    update_cache = (x₅, x₄, x₃, x₂)
+    new_obs = x₆
+    update_cache = (x₆, x₅, x₄, x₃)
+
+=#
+
+
 
 # math functions for internal use
 
