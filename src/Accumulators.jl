cafd2389b5d36f1095e3026b392ce31ac68a1b5b
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2022-12-31 21:37:08 -0500

    two versions of AccumCache

--- src/Accumulators.jl
+++ src/Accumulators.jl
@@ -9,34 +9,45 @@ export Seq, seq,
     AccMean, AccGeoMean, AccHarmMean, AccGenMean,
     AccMeanAndVar, AccMeanAndStd, AccStats,
     AccExpWtMean, AccExpWtMeanVar, AccExpWtMeanStd,
-    midrange, proportionalrange, 
+    midrange, proportionalrange,
     nminima, nmaxima
 
 #= ---- abstract types ---- =#
 
 """
     Accumulator{T,F}
-""" Accumulator
+"""
+Accumulator
 
-abstract type Accumulator{T,F}   <: Function end
+abstract type Accumulator{T,F} <: Function end
 
 """
     AccumCacher{T,F}
-""" AccumCacher
+"""
+AccumCacher
 
-abstract type AccumCacher{T,F,N} <: Accumulator{T,F} end
+abstract type AccumCacher{T,F} <: Accumulator{T,F} end
+
+"""
+    AccumCaches{T,F,N}
+"""
+AccumCaches
+
+abstract type AccumCaches{T,F,N} <: Accumulator{T,F} end
 
 #= ---- generalized sequence ---- =#
 
 """
     Seq
-""" Seq
+"""
+Seq
 
-const Seq = Union{AbstractArray{T,N}, NTuple{N,T}} where {N,T}
+const Seq = Union{AbstractArray{T,N},NTuple{N,T}} where {N,T}
 
 """
     seq
-""" seq
+"""
+seq
 
 seq(x::AbstractArray{T,N}) where {N,T} = x
 seq(x::NTuple{N,T}) where {N,T} = x
