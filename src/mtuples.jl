01889693b201f9edc74367d9c15b15e48f0cf61a
Jeffrey Sarnoff <jeffrey.sarnoff@gmail.com>
2023-01-01 01:05:01 -0500

    mtuples

---
+++ src/mtuples.jl
@@ -0,0 +1,223 @@
+
+
+to_tuple(t::Tuple) = t
+to_tuple(x::Integer) = (x,)
+to_tuple(itr) = tuple(itr...)
+
+function pad(a, b)
+    N = max(length(a), length(b))
+    Base.fill_to_length(a, 1, Val(N)), Base.fill_to_length(b, 1, Val(N))
+end
+function pad(a, b, c)
+    N = max(max(length(a), length(b)), length(c))
+    Base.fill_to_length(a, 1, Val(N)), Base.fill_to_length(b, 1, Val(N)), Base.fill_to_length(c, 1, Val(N))
+end
+
+
+
+# type-stable padding at end
+fill_tolength(t::NTuple{N,Any}, val, ::Val{N}) where {N} = t
+fill_tolength(t::Tuple{}, val, ::Val{1}) = (val,)
+fill_tolength(t::Tuple{Any}, val, ::Val{2}) = (t..., val)
+fill_tolength(t::Tuple{}, val, ::Val{2}) = (val, val)
+#function fill_to_length(t::Tuple, val, ::Val{N}) where {N}
+#    @inline
+#    return (t..., ntuple(i -> val, N - length(t))...)
+#end
+
+@inline function fill_tolength(t::Tuple, val, ::Val{_N}) where {_N}
+    M = length(t)
+    N = _N::Int
+    M > N && throw(ArgumentError("input tuple of length $M, requested $N"))
+    if @generated
+        quote
+            (t..., $(fill(:val, (_N::Int) - length(t.parameters))...))
+        end
+    else
+        (t..., fill(val, N - M)...)
+    end
+end
+
+@inline function fill_tolength(t::Tuple, val, _N::Integer)
+    fill_tolength(t::Tuple, val, Val{_N}())
+end
+
+
+# type-stable padding from start
+fill_length(t::NTuple{N,Any}, val, ::Val{N}) where {N} = t
+fill_length(t::Tuple{}, val, ::Val{1}) = (val,)
+fill_length(t::Tuple{Any}, val, ::Val{2}) = (val, t...)
+fill_length(t::Tuple{}, val, ::Val{2}) = (val, val)
+
+@inline function fill_length(t::Tuple, val, ::Val{_N}) where {_N}
+    M = length(t)
+    N = _N::Int
+    M > N && throw(ArgumentError("input tuple of length $M, requested $N"))
+    if @generated
+        quote
+            ($(fill(:val, (_N::Int) - length(t.parameters))...), t...)
+        end
+    else
+        (fill(val, N - M)..., t...)
+    end
+end
+
+@inline function fill_length(t::Tuple, val, _N::Integer)
+    fill_length(t::Tuple, val, Val{_N}())
+end
+
+# constructing from an iterator
+
+# only define these in Base, to avoid overwriting the constructors
+# NOTE: this means this constructor must be avoided in Core.Compiler!
+if nameof(@__MODULE__) === :Base
+
+    function tuple_type_tail(T::Type)
+        @_foldable_meta # TODO: this method is wrong (and not :foldable)
+        if isa(T, UnionAll)
+            return UnionAll(T.var, tuple_type_tail(T.body))
+        elseif isa(T, Union)
+            return Union{tuple_type_tail(T.a),tuple_type_tail(T.b)}
+        else
+            T.name === Tuple.name || throw(MethodError(tuple_type_tail, (T,)))
+            if isvatuple(T) && length(T.parameters) == 1
+                va = unwrap_unionall(T.parameters[1])::Core.TypeofVararg
+                (isdefined(va, :N) && isa(va.N, Int)) || return T
+                return Tuple{Vararg{va.T,va.N - 1}}
+            end
+            return Tuple{argtail(T.parameters...)...}
+        end
+    end
+
+    (::Type{T})(x::Tuple) where {T<:Tuple} = convert(T, x)  # still use `convert` for tuples
+
+    Tuple(x::Ref) = tuple(getindex(x))  # faster than iterator for one element
+    Tuple(x::Array{T,0}) where {T} = tuple(getindex(x))
+
+    (::Type{T})(itr) where {T<:Tuple} = _totuple(T, itr)
+
+    _totuple(::Type{Tuple{}}, itr, s...) = ()
+
+    function _totuple_err(@nospecialize T)
+        @noinline
+        throw(ArgumentError("too few elements for tuple type $T"))
+    end
+
+    function _totuple(::Type{T}, itr, s::Vararg{Any,N}) where {T,N}
+        @inline
+        y = iterate(itr, s...)
+        y === nothing && _totuple_err(T)
+        t1 = convert(fieldtype(T, 1), y[1])
+        # inference may give up in recursive calls, so annotate here to force accurate return type to be propagated
+        rT = tuple_type_tail(T)
+        ts = _totuple(rT, itr, y[2])::rT
+        return (t1, ts...)::T
+    end
+
+    # use iterative algorithm for long tuples
+    function _totuple(T::Type{All32{E,N}}, itr) where {E,N}
+        len = N + 32
+        elts = collect(E, Iterators.take(itr, len))
+        if length(elts) != len
+            _totuple_err(T)
+        end
+        (elts...,)
+    end
+
+    _totuple(::Type{Tuple{Vararg{E}}}, itr, s...) where {E} = (collect(E, Iterators.rest(itr, s...))...,)
+
+    _totuple(::Type{Tuple}, itr, s...) = (collect(Iterators.rest(itr, s...))...,)
+
+    # for types that `apply` knows about, just splatting is faster than collecting first
+    _totuple(::Type{Tuple}, itr::Array) = (itr...,)
+    _totuple(::Type{Tuple}, itr::SimpleVector) = (itr...,)
+    _totuple(::Type{Tuple}, itr::NamedTuple) = (itr...,)
+    _totuple(::Type{Tuple}, x::Number) = (x,) # to make Tuple(x) inferable
+
+end
+
+
+# `ntuple`, for constructing tuples of a given length
+
+"""
+    ntuple(f::Function, n::Integer)
+Create a tuple of length `n`, computing each element as `f(i)`,
+where `i` is the index of the element.
+# Examples
+```jldoctest
+julia> ntuple(i -> 2*i, 4)
+(2, 4, 6, 8)
+```
+"""
+@inline function ntuple(f::F, n::Integer) where {F}
+    # marked inline since this benefits from constant propagation of `n`
+    t = n == 0 ? () :
+        n == 1 ? (f(1),) :
+        n == 2 ? (f(1), f(2)) :
+        n == 3 ? (f(1), f(2), f(3)) :
+        n == 4 ? (f(1), f(2), f(3), f(4)) :
+        n == 5 ? (f(1), f(2), f(3), f(4), f(5)) :
+        n == 6 ? (f(1), f(2), f(3), f(4), f(5), f(6)) :
+        n == 7 ? (f(1), f(2), f(3), f(4), f(5), f(6), f(7)) :
+        n == 8 ? (f(1), f(2), f(3), f(4), f(5), f(6), f(7), f(8)) :
+        n == 9 ? (f(1), f(2), f(3), f(4), f(5), f(6), f(7), f(8), f(9)) :
+        n == 10 ? (f(1), f(2), f(3), f(4), f(5), f(6), f(7), f(8), f(9), f(10)) :
+        _ntuple(f, n)
+    return t
+end
+
+function _ntuple(f::F, n) where {F}
+    @noinline
+    (n >= 0) || throw(ArgumentError(string("tuple length should be ≥ 0, got ", n)))
+    ([f(i) for i = 1:n]...,)
+end
+
+function ntupleany(f, n)
+    @noinline
+    (n >= 0) || throw(ArgumentError(string("tuple length should be ≥ 0, got ", n)))
+    (Any[f(i) for i = 1:n]...,)
+end
+
+# inferable ntuple (enough for bootstrapping)
+ntuple(f, ::Val{0}) = ()
+ntuple(f, ::Val{1}) = (@inline; (f(1),))
+ntuple(f, ::Val{2}) = (@inline; (f(1), f(2)))
+ntuple(f, ::Val{3}) = (@inline; (f(1), f(2), f(3)))
+
+"""
+    ntuple(f, ::Val{N})
+Create a tuple of length `N`, computing each element as `f(i)`,
+where `i` is the index of the element. By taking a `Val(N)`
+argument, it is possible that this version of ntuple may
+generate more efficient code than the version taking the
+length as an integer. But `ntuple(f, N)` is preferable to
+`ntuple(f, Val(N))` in cases where `N` cannot be determined
+at compile time.
+# Examples
+```jldoctest
+julia> ntuple(i -> 2*i, Val(4))
+(2, 4, 6, 8)
+```
+"""
+@inline function ntuple(f::F, ::Val{N}) where {F,N}
+    N::Int
+    (N >= 0) || throw(ArgumentError(string("tuple length should be ≥ 0, got ", N)))
+    if @generated
+        :(@ntuple $N i -> f(i))
+    else
+        Tuple(f(i) for i = 1:N)
+    end
+end
+
+@inline function fill_to_length(t::Tuple, val, ::Val{_N}) where {_N}
+    M = length(t)
+    N = _N::Int
+    M > N && throw(ArgumentError("input tuple of length $M, requested $N"))
+    if @generated
+        quote
+            (t..., $(fill(:val, (_N::Int) - length(t.parameters))...))
+        end
+    else
+        (t..., fill(val, N - M)...)
+    end
+end
--- src/support.jl
+++ src/support.jl
@@ -2,6 +2,15 @@ const AccNum = F
 
 # shift new obs into tuple, drop oldest obs
 #=
+
+    # leftshift
+
+    # newest(cache) = last(cache)
+    # oldest(cache) = first(cache)
+    #
+    # newest obs at cache[end], oldest obs at cache[1]
+    # oldest obs at cache[1], newest obs at cache[end]
+
     init_cache   = (x₀, x₀, x₀, x₀)
     new_obs = x₁
     update_cache = (x₀, x₀, x₀, x₁)
@@ -15,7 +24,19 @@ const AccNum = F
     update_cache = (x₂, x₃, x₄, x₅)
     new_obs = x₆
     update_cache = (x₃, x₄, x₅, x₆)
+    new_obs = x₇
+    update_cache = (x₄, x₅, x₆, x₇)
+    new_obs = x₈
+    update_cache = (x₅, x₆, x₇, x₈)
 
+
+    # rightshift
+
+    # newest(cache) = first(cache)
+    # oldest(cache) = last(cache)
+    #
+    # newest obs at cache[1], oldest obs at cache[end]
+    # oldest obs at cache[end], newest obs at cache[1]
 
     init_cache   = (x₀, x₀, x₀, x₀)
     new_obs = x₁
@@ -30,14 +51,130 @@ const AccNum = F
     update_cache = (x₅, x₄, x₃, x₂)
     new_obs = x₆
     update_cache = (x₆, x₅, x₄, x₃)
+    new_obs = x₇
+    update_cache = (x₇, x₆, x₅, x₄)
+    new_obs = x₈
+    update_cache = (x₈, x₇, x₆, x₅)
+
 
 =#
 
+@inline cache_eltype(accum::AccumCaches{T,F,N}) where {T,F,N} = T
+@inline cache_length(accum::AccumCaches{T,F,N}) where {T,F,N} = N
+@inline cache_size(accum::AccumCaches{T,F,N}) where {T,F,N} = sizeof(NTuple{N,T})
+@inline cache_bits(accum::AccumCaches{T,F,N}) where {T,F,N} = cache_size(accum) * 8
+
+#=
+adaptation of code borrowed from 
+https://github.com/BioJulia/Kmers.jl/blob/master/src/tuple_bitflipping.jl
+
+
+rightshift & leftshift
+
+
+
+These methods are micro-optimised for use with an NTuple{N,T}.
+- shift out the oldest observed value, drops the [first, last]
+- shift or rewrite the youngest..penultimate_oldest values
+  -- overwriting or rewriting each value with the next younger
+  -- this leaves the youngest twice given
+  -- overwrite or replace the most extremal of the two youngest
+      with newest observed value
+
+=#
+
+@inline function dropfirst(x::NTuple{N,T}) where {N,T}
+    ntuple(i->x[i+1], N-1)
+end
+
+@inline function droplast(x::NTuple{N,T}) where {N,T}
+    ntuple(i->x[i], N-1)
+end
+
+@inline function popfirst(x::NTuple{N,T}) where {N,T}
+    x[2:end]
+end
+
+@inline function poplast(x::NTuple{N,T}) where {N,T}
+    x[1:end-1]
+end
+
+@inline function newfirst(x::NTuple{N,T}, newvalue::T) where {N,T}
+    ntuple(i->ifelse(isone(i), newvalue, x[i]), N)
+end
+
+@inline function newlast(x::NTuple{N,T}, newvalue::T) where {N,T}
+    ntuple(i->ifelse((i==N), newvalue, x[i]), N)
+end
+
+@inline function pushfirst(x::NTuple{N,T}, newvalue::T) where {N,T}
+    (newvalue, x...)
+end
+
+@inline function pushlast(x::NTuple{N,T}, newvalue::T) where {N,T}
+    (x..., newvalue)
+end
+
+
+
+@inline function rightshift1(x::NTuple{N,T}, newvalue::T) where {N,T}
+    return _rightshift1(newvalue, @view(x,2,N))
+end
+
+@inline function _rightshift1(hewvalue::T, xs::Vararg{T,N}) where {N,T}
+
+@inline function _rightshift1(xs::Vararg{T,N}) where {N,T}
+    return (xs[(head >> nbits) | carry, _rightshift_carry(nbits, (head & ((one(UInt64) << nbits) - 1)) << (64 - nbits), tail...)...)
+end
+
+@inline _rightshift1(carry::T) where {T} = ()
+
+
+#=
+These methods are micro-optimised (or should be!!!) 
+for shifting the bits in an NTuple of unsigned integers, 
+carrying the bits "shifted off" one word over to the next word.
+The carry can also be "seeded" so as other methods like
+pushfirst and pushlast can be efficiently implemented without
+duplication of code or less efficient implementations
+that first shift and then insert an element.
+
+
+rightshift_carry & leftshift_carry
+
+These methods are micro-optimised (or should be!!!) for shifting the bits in 
+an NTuple of unsigned integers, carrying the bits "shifted off" one word 
+over to the next word. The carry can also be "seeded" so as other methods like
+pushfirst and pushlast can be efficiently implemented without duplication of code
+or less efficient implementations that first shift and then insert an element.
+=#
+
+@inline function rightshift_carry(x::NTuple{N,UInt64}, nbits::Integer, prevcarry=zero(UInt64)) where {N}
+    return _rightshift_carry(nbits, prevcarry, x...)
+end
+
+@inline function _rightshift_carry(nbits::Integer, carry::UInt64, head::UInt64, tail...)
+    return ((head >> nbits) | carry, _rightshift_carry(nbits, (head & ((one(UInt64) << nbits) - 1)) << (64 - nbits), tail...)...)
+end
+
+@inline _rightshift_carry(nbits::Integer, carry::UInt64) = ()
+
+@inline function leftshift_carry(x::NTuple{N,UInt64}, nbits::Integer, prevcarry::UInt64=zero(UInt64)) where {N}
+    _, newbits = _leftshift_carry(nbits, prevcarry, x...)
+    return newbits
+end
+
+@inline function _leftshift_carry(nbits::Integer, prevcarry::UInt64, head::UInt64, tail...)
+    carry, newtail = _leftshift_carry(nbits, prevcarry, tail...)
+    return head >> (64 - nbits), ((head << nbits) | carry, newtail...)
+end
+
+@inline _leftshift_carry(nbits::Integer, prevcarry::UInt64) = prevcarry, ()
 
 
 # math functions for internal use
 
 logabs(x) = log(abs(x))
-sumlogabs(xs::A) where {T, A<:AbstractVector{T}} = vsum(map(logabs, xs))
-sumlogabs(xs::NTuple{N,T}) where {T, N} = sum(map(logabs, xs))
+sumlogabs(xs::A) where {T,A<:AbstractVector{T}} = vsum(map(logabs, xs))
+sumlogabs(xs::NTuple{N,T}) where {T,N} = sum(map(logabs, xs))
 
